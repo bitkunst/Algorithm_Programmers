4afd576546086fd7b2098d604856ef0a34b4c70c
bitkunst <bitkunst23@gmail.com>
2023-07-13 21:01:07 +0900

    [230713_study] feat: programmers 12924

--- Lv.2/12924/12924_solution.js
+++ Lv.2/12924/12924_solution.js
@@ -1,8 +1,11 @@
 function solution(n) {
     // 자연수 n을 연속한 자연수들로 표현하는 방법 찾기
-    let answer = 0;
 
-    for (let i = 1; i <= n; i++) {
+    // 자기 자신으로 표현되는 경우 미리 추가
+    let answer = 1;
+    if (n === 1) return answer;
+
+    for (let i = 1; i <= Math.ceil(n / 2); i++) {
         let sum = sumLoop(i, n);
         if (sum > n) continue;
         if (sum === n) answer += 1;
@@ -13,14 +16,16 @@ function solution(n) {
 
 function sumLoop(m, n) {
     let sum = 0;
-    for (let i = m; i <= n; i++) {
-        if (sum > n) break;
-        if (sum === n) return sum;
+    for (let i = m; i <= Math.ceil(n / 2); i++) {
+        if (sum >= n) break;
         sum += i;
     }
 
     return sum;
 }
 
-const test = solution(15);
-console.log('test', test);
+const test1 = solution(15);
+const test2 = solution(1);
+
+console.log('test1', test1);
+console.log('test2', test2);
